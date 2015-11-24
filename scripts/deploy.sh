9110933d32cbc2d9f509825320beb1a8ab4c3efb
Jim Fowler <fowler@math.osu.edu>
2015-11-24 09:40:09 -0500

    Cleaner script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-./node_modules/harp/bin/harp compile &&
-git checkout gh-pages &&
-cp -R www/js . &&
-cp www/*.html . &&
-git add www/js/* &&
-git add *.html &&
-git add photographs/*.jpg &&
-git commit -m "Deploying $(date)" &&
+./node_modules/harp/bin/harp compile
+git checkout gh-pages 
+cp -R www/js . 
+cp www/*.html . 
+git add www/js/* 
+git add *.html 
+git add photographs/*.jpg 
+git commit -m "Deploying $(date)" 
 git push origin gh-pages
