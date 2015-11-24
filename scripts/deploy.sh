ff8f88abe7346b6af5a9d331c6dd1de879de45b0
Jim Fowler <fowler@math.osu.edu>
2015-11-24 15:03:47 -0500

    Deploying CSS files from bower_components

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,11 @@
 ./node_modules/harp/bin/harp compile
 git checkout gh-pages 
 cp www/*.html .
-cp www/*.css . 
+cp www/*.css .
+cp -R www/css css
 git add *.html
 git add *.css
+git add css/*
 git add photographs/*.jpg 
 git commit -m "Deploying $(date)" 
 git push origin gh-pages
