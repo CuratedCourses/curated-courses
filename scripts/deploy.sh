a8ee9a0e8befbcb4a06192b08cfd93e18c350003
Jim Fowler <fowler@math.osu.edu>
2015-11-24 20:39:04 -0500

    Deploy js too

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,16 @@
 ./node_modules/harp/bin/harp compile
 git checkout gh-pages 
 cp www/*.html .
-cp www/*.js .
-cp www/*.css .
-cp -R www/css css
 git add *.html
+cp www/*.js .
 git add *.js
+cp www/*.css .
 git add *.css
+cp -R www/css css
 git add css/*
+cp -R www/js js
+git add js/*
+cp -R www/photographs photographs
 git add photographs/*.jpg 
 git commit -m "Deploying $(date)" 
 git push origin gh-pages
